b97c02da36155c4c41e28f8a7f115310d05b480b
devanshdixit <devanshdixit01@gmail.com>
2023-04-17 16:37:59 +0530

    first commit2

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -11,6 +11,18 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const path = pathArr.join("/");
   const fbclid = ctx.query.fbclid;
 
+	//redirect if twitter
+	if (referringURL?.includes("twitter.com") || fbclid) {
+		return {
+		  redirect: {
+			permanent: false,
+			destination: `${
+			  endpoint.replace(/(\/graphql\/)/, "/") + encodeURI(path as string)
+			}`,
+		  },
+		};
+	  }
+
   // redirect if facebook is the referer or request contains fbclid
   if (referringURL?.includes("facebook.com") || fbclid) {
     return {
